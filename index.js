fedad0744e7e50fea8e5316f42db4f2db1a8929e
TDL2627 <the.dark.lord.2627@gmail.com>
2022-02-23 11:49:26 +0200

    update

--- index.js
+++ index.js
@@ -23,128 +23,128 @@ app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.send({
-    message: "Welcome to my nike api API",
-    user_routes: {
-      user_register: {
-        method: "POST",
-        route: "/users",
-        request_body: {
-          name: "String",
-          email: "String",
-          contact: "String",
-          password: "String",
-        },
-        result: {
-          jwt: "String token",
-        },
-      },
-      user_login: {
-        method: "PATCH",
-        route: "/users",
-        request_body: {
-          email: "String",
-          password: "String",
-        },
-        result: {
-          jwt: "String token",
-        },
-      },
-      all_users: {
-        method: "GET",
-        route: "/users",
-        result: {
-          users: "Array",
-        },
-      },
-      single_user: {
-        method: "GET",
-        route: "/users/:id",
-        result: {
-          user: "Object",
-        },
-      },
-      update_user: {
-        method: "PUT",
-        request_body: {
-          name: "String",
-          email: "String",
-          contact: "String",
-          password: "String",
-          avatar: "String",
-          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-        },
-        route: "/users/:id",
-        result: {
-          user: "Object",
-        },
-      },
-      delete_user: {
-        method: "DELETE",
-        route: "/users/:id",
-        result: {
-          message: "Object",
-        },
-      },
-    },
-    post_routes: {
-      all_posts: {
-        method: "GET",
-        route: "/posts",
-        headers: {
-          authorization: "Bearer (JWT token)",
-        },
-        result: {
-          posts: "Array",
-        },
-      },
-      single_post: {
-        method: "GET",
-        route: "/posts/:id",
-        headers: {
-          authorization: "Bearer (JWT token)",
-        },
-        result: {
-          post: "Object",
-        },
-      },
-      create_post: {
-        method: "POST",
-        route: "/posts/",
-        headers: {
-          authorization: "Bearer (JWT token)",
-        },
-        request_body: {
-          title: "String",
-          body: "String",
-          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-        },
-        result: {
-          post: "Object",
-        },
-      },
-      update_post: {
-        method: "PUT",
-        route: "/posts/:id",
-        headers: {
-          authorization: "Bearer (JWT token)",
-        },
-        request_body: {
-          title: "String *optional*",
-          body: "String *optional*",
-          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-        },
-        result: {
-          post: "Object",
-        },
-      },
-      delete_post: {
-        method: "DELETE",
-        route: "/posts/:id",
-        result: {
-          message: "Object",
-        },
-      },
-    },
+    message: "Welcome to my nike api API"
+    // user_routes: {
+    //   user_register: {
+    //     method: "POST",
+    //     route: "/users",
+    //     request_body: {
+    //       name: "String",
+    //       email: "String",
+    //       contact: "String",
+    //       password: "String",
+    //     },
+    //     result: {
+    //       jwt: "String token",
+    //     },
+    //   },
+    //   user_login: {
+    //     method: "PATCH",
+    //     route: "/users",
+    //     request_body: {
+    //       email: "String",
+    //       password: "String",
+    //     },
+    //     result: {
+    //       jwt: "String token",
+    //     },
+    //   },
+    //   all_users: {
+    //     method: "GET",
+    //     route: "/users",
+    //     result: {
+    //       users: "Array",
+    //     },
+    //   },
+    //   single_user: {
+    //     method: "GET",
+    //     route: "/users/:id",
+    //     result: {
+    //       user: "Object",
+    //     },
+    //   },
+    //   update_user: {
+    //     method: "PUT",
+    //     request_body: {
+    //       name: "String",
+    //       email: "String",
+    //       contact: "String",
+    //       password: "String",
+    //       avatar: "String",
+    //       img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+    //     },
+    //     route: "/users/:id",
+    //     result: {
+    //       user: "Object",
+    //     },
+    //   },
+    //   delete_user: {
+    //     method: "DELETE",
+    //     route: "/users/:id",
+    //     result: {
+    //       message: "Object",
+    //     },
+    //   },
+    // },
+    // post_routes: {
+    //   all_posts: {
+    //     method: "GET",
+    //     route: "/posts",
+    //     headers: {
+    //       authorization: "Bearer (JWT token)",
+    //     },
+    //     result: {
+    //       posts: "Array",
+    //     },
+    //   },
+    //   single_post: {
+    //     method: "GET",
+    //     route: "/posts/:id",
+    //     headers: {
+    //       authorization: "Bearer (JWT token)",
+    //     },
+    //     result: {
+    //       post: "Object",
+    //     },
+    //   },
+    //   create_post: {
+    //     method: "POST",
+    //     route: "/posts/",
+    //     headers: {
+    //       authorization: "Bearer (JWT token)",
+    //     },
+    //     request_body: {
+    //       title: "String",
+    //       body: "String",
+    //       img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+    //     },
+    //     result: {
+    //       post: "Object",
+    //     },
+    //   },
+    //   update_post: {
+    //     method: "PUT",
+    //     route: "/posts/:id",
+    //     headers: {
+    //       authorization: "Bearer (JWT token)",
+    //     },
+    //     request_body: {
+    //       title: "String *optional*",
+    //       body: "String *optional*",
+    //       img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+    //     },
+    //     result: {
+    //       post: "Object",
+    //     },
+    //   },
+    //   delete_post: {
+    //     method: "DELETE",
+    //     route: "/posts/:id",
+    //     result: {
+    //       message: "Object",
+    //     },
+    //   },
+    // },
   });
 });
 app.use("/users", userRouter);
