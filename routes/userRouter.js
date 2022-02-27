f93fb835ef92fa15c4259bd7c435464fa6ce807d
TDL2627 <the.dark.lord.2627@gmail.com>
2022-02-27 16:58:33 +0200

    added a gender op

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -51,7 +51,7 @@ router.patch("/", async (req, res, next) => {
 
 // REGISTER a user
 router.post("/", async (req, res, next) => {
-  const { name, email, contact, password } = req.body;
+  const { name, email, contact, password, about } = req.body;
 
   const salt = await bcrypt.genSalt();
   const hashedPassword = await bcrypt.hash(password, salt);
@@ -60,6 +60,7 @@ router.post("/", async (req, res, next) => {
     name,
     email,
     contact,
+    about,
     password: hashedPassword,
   });
 
